53dfaf44cb226724cc393a8ea916049b9874e99c
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-05-31 23:30:51 -0500

    Updates

---
+++ precache-manifest.da522dc8a3035cd5ef4aa24da4c8cedf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ee63520b9b483a4601eefa013838df",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "977b3a3eaf137955c78b",
+    "url": "/Markdown-Previewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "efd885f188321bd2246f",
+    "url": "/Markdown-Previewer/static/css/main.f2f97ba8.chunk.css"
+  },
+  {
+    "revision": "977b3a3eaf137955c78b",
+    "url": "/Markdown-Previewer/static/js/2.db06fb93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.db06fb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd885f188321bd2246f",
+    "url": "/Markdown-Previewer/static/js/main.c1ff1c8b.chunk.js"
+  },
+  {
+    "revision": "e79790518aa5a53e5d98",
+    "url": "/Markdown-Previewer/static/js/runtime-main.2c3dfcb3.js"
+  }
+]);
